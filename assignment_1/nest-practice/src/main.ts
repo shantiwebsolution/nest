a2142164ab22c6ec8672c201da1116c49333c768
Jayesh Dhudashia <prince.swami@gmail.com>
2025-09-24 18:41:53 +0530

    Vercel Dployeement supporrt

--- assignment_1/nest-practice/src/main.ts
+++ assignment_1/nest-practice/src/main.ts
@@ -1,8 +1,14 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+let app: INestApplication;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
+
+export { app };
